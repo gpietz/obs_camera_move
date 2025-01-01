a5b43b64556754b51f91b98926b5cd86e69fa295
Citiga <77841571+gpietz@users.noreply.github.com>
2025-01-01 13:50:17 +0100

    Namespace description shortened

--- src/library.cpp
+++ src/library.cpp
@@ -8,9 +8,9 @@ std::unique_ptr<ObsCamMove::TCPServer> tcp_server;
 
 bool obs_module_load() {
     try {
-        ocm::log(ocm::LogLevel::INFO, "OBS Camera Move loaded!");
-        const auto tcp_port = ObsCamMove::get_env_var_int("OBS_CAMERA_MOVE_PORT", 5680);
-        tcp_server = std::make_unique<ObsCamMove::TCPServer>(tcp_port);
+        ocm::log(ocm::LogLevel::INFO, "**** OBS Camera Move loading ****");
+        const auto tcp_port = ocm::get_env_var_int("OBS_CAMERA_MOVE_PORT", 5680);
+        tcp_server = std::make_unique<ocm::TCPServer>(tcp_port);
         tcp_server->start();
     } catch (const std::exception &e) {
         ocm::log(ocm::LogLevel::ERROR, std::string("Exception occurred while loading plugin: ") + e.what());
